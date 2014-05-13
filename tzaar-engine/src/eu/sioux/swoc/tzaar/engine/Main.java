ac598c3d5661765a8c7c000bff0ef95957b4bafa
Ralph Schiedon <ralph.schiedon@sioux.eu>
2014-05-13 20:03:44 +0200

    Add initial code that starts two executables passed as command line parameters.

---
+++ tzaar-engine/src/eu/sioux/swoc/tzaar/engine/Main.java
@@ -0,0 +1,20 @@
+package eu.sioux.swoc.tzaar.engine;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		String bot1Executable = args[0];
+		String bot2Executable = args[1];
+
+		try (EngineRunner runner = new EngineRunner(bot1Executable, bot2Executable))
+		{
+			runner.run();
+		}
+		catch (Exception ex)
+		{
+			System.out.println("Error: " + ex.getMessage());
+		}
+	}
+
+}
---
+++ tzaar-engine/src/eu/sioux/swoc/tzaar/engine/io/IORobot.java
@@ -0,0 +1,79 @@
+// Copyright 2014 theaigames.com (developers@theaigames.com)
+
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+
+//        http://www.apache.org/licenses/LICENSE-2.0
+
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//	
+//    For the full copyright and license information, please view the LICENSE
+//    file that was distributed with this source code.
+
+package eu.sioux.swoc.tzaar.engine.io;
+
+import java.io.IOException;
+
+public class IORobot implements AutoCloseable
+{
+	IOHandler handler;
+	StringBuilder dump;
+	int errorCounter;
+	final int maxErrors = 2;
+	
+	public IORobot(String command) throws IOException
+	{
+		handler = new IOHandler(command);
+		dump = new StringBuilder();
+		errorCounter = 0;
+	}
+	
+	public void setup(long timeOut)
+	{
+	}
+	
+	public String doMove(long timeOut)
+	{
+//		String output = "pick_starting_regions " + timeOut;
+//		for(Region region : pickableRegions)
+//			output = output.concat(" " + region.getId());
+//
+		String output = "Do your job, idiot";
+		handler.writeLine(output);
+		String line = handler.readLine(timeOut);
+		dump.append(output + "\n");
+		dump.append(line + "\n");
+		return line;
+	}
+	
+	@Override
+	public void close()
+	{
+		handler.close();
+	}
+	
+	public String getStdin()
+	{
+		return handler.getStdin();
+	}
+	
+	public String getStdout()
+	{
+		return handler.getStdout();
+	}
+	
+	public String getStderr()
+	{
+		return handler.getStderr();
+	}
+
+	public String getDump()
+	{
+		return dump.toString();
+	}
+}
