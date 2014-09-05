247541b94e17c9f69448f2d71f221967c83f4415
Ralph Schiedon <drZymo@users.noreply.github.com>
2014-09-05 10:03:03 +0200

    Add two bots.

--- gos-engine/src/gos/engine/Main.java
+++ gos-engine/src/gos/engine/Main.java
@@ -4,7 +4,9 @@ import java.net.UnknownHostException;
 import java.util.List;
 import java.util.Set;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
+import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 
 public class Main
@@ -15,25 +17,52 @@ public class Main
         {
             MongoClient client = new MongoClient("localhost");
 
-            List<String> dbNames = client.getDatabaseNames();
-            System.out.println(dbNames.size() + " databases");
-            for (String dbName : dbNames)
-            {
-                System.out.println(dbName);
-
-                DB db = client.getDB(dbName);
-                Set<String> tables = db.getCollectionNames();
-                System.out.println("  " + tables.size() + " tables");
-
-                for (String coll : tables)
-                {
-                    System.out.println("    " + coll);
-                }
-            }
+            DB db = client.getDB("swoc-dev");
+            
+            DumpAllTables(db);
+            
+            MakeABot(db, "Bot One");
+            MakeABot(db, "Bot Two");
+            
             client.close();
         } catch (UnknownHostException ex)
         {
             ex.printStackTrace();
+        }
+    }
+    
+    private static void MakeABot(DB db, String botName)
+    {
+        DBCollection table = db.getCollection("Bot");
+        BasicDBObject document = new BasicDBObject();
+        document.put("name", botName);
+        document.put("ranking", 0);
+        document.put("version", 1);
+        document.put("executablePath", "/home/swocslave/test/swoc2014/SharpBot/Bin/SharpBot.exe");
+        table.insert(document);
+    }
+
+    private static void DumpAllDatabases(MongoClient client)
+    {
+        List<String> dbNames = client.getDatabaseNames();
+        System.out.println(dbNames.size() + " databases");
+        for (String dbName : dbNames)
+        {
+            System.out.println(dbName);
+
+            DB db = client.getDB(dbName);
+            DumpAllTables(db);
+        }
+    }
+
+    private static void DumpAllTables(DB db)
+    {
+        Set<String> tables = db.getCollectionNames();
+        System.out.println("  " + tables.size() + " tables");
+
+        for (String coll : tables)
+        {
+            System.out.println("    " + coll);
         }
     }
 
