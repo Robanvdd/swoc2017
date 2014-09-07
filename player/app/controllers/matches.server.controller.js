15bef2a1885f73a537dcabbed471c11bab823565
Robbert-Jan van der Dennen <robbertjanv@gmail.com>
2014-09-07 12:32:16 +0200

    fix bot names in Game log

--- player/app/controllers/matches.server.controller.js
+++ player/app/controllers/matches.server.controller.js
@@ -5,9 +5,9 @@ var mongoose = require('mongoose'),
 	Match = mongoose.model('Match');
 
 exports.retrieveAll = function(callback) {
-	Match.find({}, callback);
+	Match.find({}.populate('blackBot', 'name').populate('whiteBot', 'name').populate('winnerBot', 'name'), callback);
 }
 
 exports.retrieveById = function(id, callback) {
-	Match.findOne({_id:id}, callback);
+	Match.findOne({_id:id}).populate('blackBot', 'name').populate('whiteBot', 'name').populate('winnerBot', 'name'), callback);
 }
