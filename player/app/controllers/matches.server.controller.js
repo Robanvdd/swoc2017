b9192d13ce771d42d4d481ca19f057babaa0c3e2
Robbert-Jan van der Dennen <robbertjanv@gmail.com>
2014-09-07 16:21:22 +0200

    another fix for botnames

--- player/app/controllers/matches.server.controller.js
+++ player/app/controllers/matches.server.controller.js
@@ -5,9 +5,9 @@ var mongoose = require('mongoose'),
 	Match = mongoose.model('Match');
 
 exports.retrieveAll = function(callback) {
-	Match.find({}).populate('blackBot', 'name').populate('whiteBot', 'name').populate('winnerBot', 'name', callback);
+	Match.find({}).populate('blackBot', 'name').populate('whiteBot', 'name').populate('winnerBot', 'name').exec(callback);
 }
 
 exports.retrieveById = function(id, callback) {
-	Match.findOne({_id:id}).populate('blackBot', 'name').populate('whiteBot', 'name').populate('winnerBot', 'name', callback);
+	Match.findOne({_id:id}).populate('blackBot', 'name').populate('whiteBot', 'name').populate('winnerBot', 'name').exec(callback);
 }
