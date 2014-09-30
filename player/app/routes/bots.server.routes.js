31c0abbb35f961f87f6daa966eff3b33b19f46a7
JaWSnl <jw.van.silfhout@gmail.com>
2014-09-30 18:48:28 +0200

    Disable leaderboard before powwow

--- player/app/routes/bots.server.routes.js
+++ player/app/routes/bots.server.routes.js
@@ -3,16 +3,16 @@
  */
 var users = require('../../app/controllers/users'),
 	bots = require('../../app/controllers/bots'),
-	path = require('path')
+	path = require('path'),
 	fs = require('fs');
 
 module.exports = function(app) {
 
 	app.post('/api/bot/upload/', users.requiresLogin, bots.upload);
-	app.get('/api/bot/getactivebots/', bots.retrieveAllLatest);
+	app.get('/api/bot/getactivebots/', users.requiresAdmin, bots.retrieveAllLatest);
     app.get('/api/bot/getoldbots/', users.requiresLogin, bots.retrieveOldBots)
 
-    app.get('/api/bot/history/:username', function(req, res) {
+    app.get('/api/bot/history/:username', users.requiresAdmin, function(req, res) {
         bots.retrieveHistory(req, res, req.params.username);
     });
 }
