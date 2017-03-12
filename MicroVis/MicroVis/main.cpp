8cc40d13c1b20638c9d9fd9e29553ef9d790f830
Arjan Broos <Arjan Broos>
2017-03-12 21:33:11 +0100

    Add C++ json lib for MicroVis

--- MicroVis/MicroVis/main.cpp
+++ MicroVis/MicroVis/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include "json.hpp"
+#include <fstream>
+
+using json = nlohmann::json;
 
 int main()
 {
-	std::cout << "Test" << std::endl;
+	std::cout << "JSON file read:" << std::endl;
+	std::ifstream input("input.json");
+	json jsonInput;
+	input >> jsonInput;
+
+	std::cout << jsonInput["positions"];
+
+	std::cout << jsonInput << std::endl;
+
 	return 0;
 }
