a2af46ff6ecefefde98e89235d6d4cbe5c24b308
Arjan Broos <Arjan Broos>
2017-05-10 20:28:00 +0200

    Add SFML. Add window for 2D graphics to MicroVis (Windows only, for now)

--- MicroVis/src/MicroVis/cpp/main.cpp
+++ MicroVis/src/MicroVis/cpp/main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 #include "json.hpp"
 #include <fstream>
+#include <SFML/Graphics.hpp>
 
 using json = nlohmann::json;
 
 int main()
 {
+	std::cout << "Starting SFML stuff" << std::endl;
+	sf::RenderWindow window(sf::VideoMode(400,400), "Hello World!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
 	const std::string gameFilename("microgame1.json");
 	std::cout << "Looking for " << gameFilename << std::endl;
 
