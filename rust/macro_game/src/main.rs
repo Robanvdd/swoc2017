10b873c1fe07e051c16929fcb4cac42ec114810f
Mibe <Michael@mike.local>
2017-05-23 23:18:37 +0200

    added micro_game

--- rust/macro_game/src/main.rs
+++ rust/macro_game/src/main.rs
@@ -13,20 +13,29 @@ use std::process::{Command, Stdio};
 use std::error::Error;
 use std::io::prelude::*;
 
-fn f()
+fn start()
 {
-    let systems = game::GameObject::default();
-    systems.save_game_object_json();
+    //init a gameobj
+    let tempGameObj = game::GameObject::default();
+    
+    //start micro game and send the gameobj
+    let mut micro = GameProcess::new("/Users/Michael/Documents/swoc/swoc2017/rust/micro_game/target/debug/micro_game".to_string(), "".to_string());
+    micro.send_input(&tempGameObj);
+    //receive the gameobj
+    let systemsDone = micro.get_output();
+
+    //save gameobj for vis.
+    systemsDone.save_game_object_json();
+
+    // save some stats
     let stats = game_stats::GameStats{ BotOne: 1, BotTwo: 2 };
     stats.save();
 }
 
 fn main() {
-
-    let micro = GameProcess::new("python".to_string(), "/Users/Michael/Documents/swoc/swoc2017/rust/macro_game/test.py".to_string());
-
+    ////swoc/swoc2017/rust/macro_game/test.py
     
-
+    //start a python bot
     let process = match Command::new("python")
                                 .arg("/Users/Michael/Documents/swoc/swoc2017/rust/macro_game/test.py")
                                 .stdin(Stdio::piped())
@@ -36,9 +45,10 @@ fn main() {
         Ok(process) => process,
     };
 
+    //receive a instruction from python script
     let mut s = String::new();
     match process.stdout.unwrap().read_to_string(&mut s) {
         Err(why) => panic!("couldn't read wc stdout: {}", why.description()),
-        _ => f(),
+        _ => start(),
     }
 }
---
+++ rust/micro_game/src/main.rs
@@ -0,0 +1,21 @@
+extern crate game_structures;
+extern crate game_logic;
+extern crate game_process;
+
+use game_structures::game;
+use game_structures::game::GameObjectFunctions;
+use game_logic::game_stats;
+
+use game_process::process::GameProcess;
+
+fn main() {
+    //wait for gameObj (sdtin)
+
+    //let gameObj = GameProcess::get_output();
+    
+    //modify a value in gameobj
+    
+    //send back the gameObj
+    
+    println!("Hello, world!");
+}
