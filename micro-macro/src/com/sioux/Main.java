0db11e27e55898e23da4f3a114b621a1a42d2e72
Mibe <Michael@mike.local>
2017-06-28 18:32:01 +0200

    added io macro

--- micro-macro/src/com/sioux/Main.java
+++ micro-macro/src/com/sioux/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        // 1 create a game object
+        // 2 start the macro game engine and send the object to the game engine
+        // 3
     }
 }
---
+++ micro-macro/src/com/sioux/game_engine/MacroEngine.java
@@ -0,0 +1,8 @@
+package com.sioux.game_engine;
+
+/**
+ * Created by Michael on 21/06/2017.
+ */
+public class MacroEngine {
+
+}
