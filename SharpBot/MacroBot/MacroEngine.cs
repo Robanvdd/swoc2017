aa746585594b79158afff29485f142a248088af2
Dwight Berendse <dwight.berendse@sioux.eu>
2017-09-21 20:00:16 +0200

    Basic macro bot for testing. Added polar/cartesian coord conversion support

---
+++ SharpBot/MacroBot/MacroEngine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MacroBot.Protocol;
+
+namespace MacroBot
+{
+    public sealed class MacroEngine : Engine
+    {
+        public override void Response(GameState gameState)
+        {
+            var mePlayer = gameState.Players.Single(player => player.Name == "dwight");
+            var planet0 = gameState.SolarSystems.SelectMany(sys => sys.Planets).First();
+
+            WriteMessage(new Protocol.GameResponseBuy
+            {
+                Amount = 1,
+                PlanetId = planet0.Id,
+            });
+
+            var ufo = mePlayer.Ufos.First();
+            WriteMessage(new Protocol.GameResponseMoveToPlanet
+            {
+                PlanetId = planet0.Id,
+                Ufos = new List<int> { ufo.Id },
+            });
+        }
+    }
+}
---
+++ SharpBot/MacroBot/Protocol/GameResponseMoveToPlanet.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MacroBot.Protocol
+{
+    public sealed class GameResponseMoveToPlanet
+    {
+        public string Command { get { return "moveToPlanet"; } }
+        public List<int> Ufos { get; set; }
+        public int PlanetId { get; set; }
+
+        public string ToJson(Formatting formatting = Formatting.None)
+        {
+            return JsonConvert.SerializeObject(this, formatting);
+        }
+    }
+}
