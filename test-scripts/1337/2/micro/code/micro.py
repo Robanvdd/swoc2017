2bbce6b43736547f5d6f42b18fa35f9862715429
Arjan Broos <arjanbroos@live.nl>
2017-10-04 17:39:08 +0200

    Add .user back to gitignore + update scripts

---
+++ test-scripts/1337/2/micro/code/micro.py
@@ -0,0 +1,88 @@
+"""
+Example micro bot script.
+
+"""
+
+import sys
+import json
+import logging
+import math
+
+fh = logging.FileHandler('script.log')
+fh.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(message)s')
+fh.setFormatter(formatter)
+
+logger = logging.getLogger('script')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(fh)
+
+time = 0
+
+def game_loop():
+    # Wait for input from micro.
+    micro_json = input()
+    logger.info('input' + micro_json)
+    micro = json.loads(micro_json)
+
+    arenaWidth = micro['arena']['width']
+    arenaHeight = micro['arena']['height']
+    player = micro['playerId']
+
+    global time
+    time = time + 0.1
+    
+    bots = []
+    for p in micro['players']:
+        if p['id'] == player:
+            bots = p['bots']
+
+    otherBots = []
+    for p in micro['players']:
+        if p['id'] != player:
+            otherBots += p['bots']
+
+    targetX = 0
+    targetY = 0
+    if otherBots != []:
+        targetBot = otherBots[0]
+        targetX = targetBot['position']['x']
+        targetY = targetBot['position']['y']
+
+    commands = {
+        'commands': []
+    }
+    
+    for bot in bots:
+        commands['commands'].append({
+            'id': bot['id'],
+            'move': {
+                'direction': math.sin(time * 3) * 180,
+                'speed': math.sin(time) * 4
+            },
+#                'shoot': {
+#                    'direction': 180
+#                }
+            'shootAt': {
+                'x': targetX,
+                'y': targetY
+            }
+        })
+
+    # Send output to micro.
+    commands_json = json.dumps(commands)
+    logger.info('output' + commands_json)
+    print(commands_json)
+
+
+def main():
+    while True:
+        try:
+            game_loop()
+        except:
+            logger.exception()
+
+
+if __name__ == '__main__':
+    main()
